753d3e876058772cbb0753a26fb8444612dca493
LynnVlasenko <lynn.vlasenko@gmail.com>
2024-03-01 17:26:17 +0100

    Completed all tasks. Add a little fantasy :)

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -9,15 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var greeting: UILabel!
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var tapButton: UIButton!
+    var isTouch = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        greeting.text = "Hello!"
+        greetingLabel.text = "Hello!"
+        tapButton.layer.cornerRadius = 25
+        tapButton.clipsToBounds = true
     }
 
-
+    @IBAction func tapMeButtonAction(_ sender: UIButton) {
+        if !isTouch {
+            greetingLabel.text = "Welcome to the CHI Academy!👩🏻‍💻"
+            greetingLabel.numberOfLines = 3
+            isTouch = true
+        } else {
+            greetingLabel.text = "Hello!"
+            isTouch = false
+        }
+    }
+    
 }
 
